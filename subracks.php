885f43c67624f5148c96a33b86f056742b5b7ad4
chriskout <34226682+chriskout@users.noreply.github.com>
2019-09-22 21:59:35 -0400

    Update subracks.php

--- subracks.php
+++ subracks.php
@@ -1,24 +1,24 @@
 <?php
 header('Content-Type: application/json');
 $aResult = array();
-$myrack = 'arguments'[0];
+
 
 $racks = [];
 
-if( !isset($_POST['subracks']) ) { $racks['error'] = 'No function name!'; }
+if( !isset($_GET['subracks']) ) { $racks['error'] = 'No function name!'; }
 
-if( !isset($_POST['arguments']) ) { $racks['error'] = 'No function arguments!'; }
+if( !isset($_GET['arguments']) ) { $racks['error'] = 'No function arguments!'; }
 
 if( !isset($racks['error']) ) {
 
-        switch($_POST['subracks']) {
+        switch($_GET['subracks']) {
 		case 'sub':
-		 if( !is_array($_POST['arguments']) || (count($_POST['arguments']) < 2) ) {
+		 if( !is_array($_GET['arguments']) || (count($_GET['arguments']) < 1) ) {
                    $racks['error'] = 'Error in arguments!';
                }
 		else {   
 
-
+$myrack = 'arguments'[0];
 for($i = 0; $i < pow(2, strlen($myrack)); $i++){
 
 	$ans = "";
